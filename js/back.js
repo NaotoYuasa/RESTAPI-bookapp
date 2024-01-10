302d789a4d31a6ee223fd4846b2653d71c17f1ae
yuasa <y.naoto.122518@gmail.com>
2024-01-10 16:09:56 +0900

    戻るボタン完了

--- js/back.js
+++ js/back.js
@@ -1,6 +1,6 @@
-document.getElementById('backButton').addEventListener('click', goBack);
-
-        function goBack() {
-            history.back();
-            location.reload();
-        }
+window.addEventListener('pageshow', () => {
+    if(performance.getEntriesByType("navigation")[0].type === 'back_forward'){
+        alert('ブラウザの「戻る」または「進む」でページが表示されました。ページをリロードします。');
+        location.reload();
+    }
+});
