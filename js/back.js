c526e4bfae29884cf40a574c5b4212917cc319a8
yuasa <y.naoto.122518@gmail.com>
2024-01-10 15:24:57 +0900

    途中

--- js/back.js
+++ js/back.js
@@ -1,6 +1,6 @@
-function goBack() {
-    history.back();
-    console.log('Before reload');
-    location.reload(); // ロード
-    console.log('After reload');
-}
+document.getElementById('backButton').addEventListener('click', goBack);
+
+        function goBack() {
+            history.back();
+            location.reload();
+        }
